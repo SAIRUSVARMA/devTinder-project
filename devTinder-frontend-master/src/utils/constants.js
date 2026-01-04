f83cf2b3a33e1da53df425e9e257c4295ed0d73b
SAIRUS VARMA <sairusvarma201234@gmail.com>
2026-01-05 04:47:12 +0530

    yo

--- devTinder-frontend-master/src/utils/constants.js
+++ devTinder-frontend-master/src/utils/constants.js
@@ -3,4 +3,4 @@ const NODE_ENV = import.meta.env.MODE || "development";
 export const BASE_URL =
   NODE_ENV === "development"
     ? "http://localhost:5000" // backend local server
-    : "https://your-production-backend.com"; // replace with your deployed backend
+    : "https://devtinder-project-1.onrender.com"; // replace with your deployed backend
